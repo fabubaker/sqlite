399551762992f77c900403e5634c4a2ca6fcad4d
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2022-12-08 12:47:46 -0500

    Complete implementation of vdbe-jit

--- src/vdbe-jit.c
+++ src/vdbe-jit.c
@@ -1,6 +1,9 @@
 #include "sqliteInt.h"
 #include "vdbeInt.h"
 #include <jit/jit.h>
+#include <time.h>
+
+typedef void (*FF) (void);
 
 jit_context_t setupJIT() {
     // Setup LibJIT
@@ -56,6 +59,14 @@ jit_function_t sqlite3VdbeJITExec(Vdbe* p, jit_context_t context) {
                 // Stuff integer value in P1 into register P2
                 jit_value_t p1 = jit_value_create_nint_constant(function, jit_type_long, pOp->p1);
                 jit_insn_store_relative(function, JIT_registers[pOp->p2], 0, p1);
+                MemSetTypeFlag(&p->aMem[pOp->p2], MEM_Int);
+                break;
+            }
+            case OP_Real: {
+                // Stuff real value in P4 into register P2
+                jit_value_t p4 = jit_value_create_float64_constant(function, jit_type_float64, *pOp->p4.pReal);
+                jit_insn_store_relative(function, JIT_registers[pOp->p2], 0, p4);
+                MemSetTypeFlag(&p->aMem[pOp->p2], MEM_Real);
                 break;
             }
             case OP_Goto: {
@@ -66,10 +77,178 @@ jit_function_t sqlite3VdbeJITExec(Vdbe* p, jit_context_t context) {
             case OP_Add: {
                 // Add the value in register P1 to the value in register P2
                 // and store result in register P3.
-                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, jit_type_long);
-                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, jit_type_long);
+                jit_type_t p1_type;
+                jit_type_t p2_type;
+
+                // Perform compile-time type checking of P1 and P2
+                if ((p->aMem[pOp->p1].flags & MEM_Int) != 0) {
+                    p1_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p1].flags & MEM_Real) != 0) {
+                    p1_type = jit_type_float64;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Int) != 0) {
+                    p2_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Real) != 0) {
+                    p2_type = jit_type_float64;
+                }
+
+                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, p1_type);
+                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, p2_type);
                 jit_value_t tmp = jit_insn_add(function, p1, p2);
                 jit_insn_store_relative(function, JIT_registers[pOp->p3], 0, tmp);
+
+                // Perform compile-time type checking of P3
+                jit_type_t tmp_type = jit_value_get_type(tmp);
+                int tmp_type_kind = jit_type_get_kind(tmp_type);
+                if (tmp_type_kind == JIT_TYPE_INT || tmp_type_kind == JIT_TYPE_LONG) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Int);
+                }
+                if (tmp_type_kind == JIT_TYPE_FLOAT64) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Real);
+                }
+
+                break;
+            }
+            case OP_Subtract: {
+                jit_type_t p1_type;
+                jit_type_t p2_type;
+
+                // Perform compile-time type checking of P1 and P2
+                if ((p->aMem[pOp->p1].flags & MEM_Int) != 0) {
+                    p1_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p1].flags & MEM_Real) != 0) {
+                    p1_type = jit_type_float64;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Int) != 0) {
+                    p2_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Real) != 0) {
+                    p2_type = jit_type_float64;
+                }
+
+                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, p1_type);
+                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, p2_type);
+                jit_value_t tmp = jit_insn_sub(function, p2, p1);
+                jit_insn_store_relative(function, JIT_registers[pOp->p3], 0, tmp);
+
+                // Perform compile-time type checking of P3
+                jit_type_t tmp_type = jit_value_get_type(tmp);
+                int tmp_type_kind = jit_type_get_kind(tmp_type);
+                if (tmp_type_kind == JIT_TYPE_INT || tmp_type_kind == JIT_TYPE_LONG) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Int);
+                }
+                if (tmp_type_kind == JIT_TYPE_FLOAT64) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Real);
+                }
+
+                break;
+            }
+            case OP_Multiply: {
+                jit_type_t p1_type;
+                jit_type_t p2_type;
+
+                // Perform compile-time type checking of P1 and P2
+                if ((p->aMem[pOp->p1].flags & MEM_Int) != 0) {
+                    p1_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p1].flags & MEM_Real) != 0) {
+                    p1_type = jit_type_float64;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Int) != 0) {
+                    p2_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Real) != 0) {
+                    p2_type = jit_type_float64;
+                }
+
+                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, p1_type);
+                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, p2_type);
+                jit_value_t tmp = jit_insn_mul(function, p1, p2);
+                jit_insn_store_relative(function, JIT_registers[pOp->p3], 0, tmp);
+
+                // Perform compile-time type checking of P3
+                jit_type_t tmp_type = jit_value_get_type(tmp);
+                int tmp_type_kind = jit_type_get_kind(tmp_type);
+                if (tmp_type_kind == JIT_TYPE_INT || tmp_type_kind == JIT_TYPE_LONG) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Int);
+                }
+                if (tmp_type_kind == JIT_TYPE_FLOAT64) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Real);
+                }
+
+                break;
+            }
+            case OP_Divide: {
+                jit_type_t p1_type;
+                jit_type_t p2_type;
+
+                // Perform compile-time type checking of P1 and P2
+                if ((p->aMem[pOp->p1].flags & MEM_Int) != 0) {
+                    p1_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p1].flags & MEM_Real) != 0) {
+                    p1_type = jit_type_float64;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Int) != 0) {
+                    p2_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Real) != 0) {
+                    p2_type = jit_type_float64;
+                }
+
+                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, p1_type);
+                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, p2_type);
+                jit_value_t tmp = jit_insn_div(function, p1, p2);
+                jit_insn_store_relative(function, JIT_registers[pOp->p3], 0, tmp);
+
+                // Perform compile-time type checking of P3
+                jit_type_t tmp_type = jit_value_get_type(tmp);
+                int tmp_type_kind = jit_type_get_kind(tmp_type);
+                if (tmp_type_kind == JIT_TYPE_INT || tmp_type_kind == JIT_TYPE_LONG) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Int);
+                }
+                if (tmp_type_kind == JIT_TYPE_FLOAT64) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Real);
+                }
+
+                break;
+            }
+            case OP_Remainder: {
+                jit_type_t p1_type;
+                jit_type_t p2_type;
+
+                // Perform compile-time type checking of P1 and P2
+                if ((p->aMem[pOp->p1].flags & MEM_Int) != 0) {
+                    p1_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p1].flags & MEM_Real) != 0) {
+                    p1_type = jit_type_float64;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Int) != 0) {
+                    p2_type = jit_type_long;
+                }
+                if ((p->aMem[pOp->p2].flags & MEM_Real) != 0) {
+                    p2_type = jit_type_float64;
+                }
+
+                jit_value_t p1 = jit_insn_load_relative(function, JIT_registers[pOp->p1], 0, p1_type);
+                jit_value_t p2 = jit_insn_load_relative(function, JIT_registers[pOp->p2], 0, p2_type);
+                jit_value_t tmp = jit_insn_rem(function, p1, p2);
+                jit_insn_store_relative(function, JIT_registers[pOp->p3], 0, tmp);
+
+                // Perform compile-time type checking of P3
+                jit_type_t tmp_type = jit_value_get_type(tmp);
+                int tmp_type_kind = jit_type_get_kind(tmp_type);
+                if (tmp_type_kind == JIT_TYPE_INT || tmp_type_kind == JIT_TYPE_LONG) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Int);
+                }
+                if (tmp_type_kind == JIT_TYPE_FLOAT64) {
+                    MemSetTypeFlag(&p->aMem[pOp->p3], MEM_Real);
+                }
+
                 break;
             }
             case OP_ResultRow: {
@@ -77,7 +256,6 @@ jit_function_t sqlite3VdbeJITExec(Vdbe* p, jit_context_t context) {
                 p->cacheCtr = (p->cacheCtr + 2)|1;
                 p->pResultSet = &p->aMem[pOp->p1];
                 p->pc = (int)(pOp - aOp) + 1;
-                MemSetTypeFlag(&p->aMem[1], MEM_Int);
                 break;
             }
             default: {
@@ -87,18 +265,31 @@ jit_function_t sqlite3VdbeJITExec(Vdbe* p, jit_context_t context) {
     }
     exit_loop: ;
 
-    jit_dump_function(stdout, function, "union");
+    // Uncomment this to see LibJIT IR
+    printf("<Debug> Printing LibJIT IR...\n");
+    jit_dump_function(stdout, function, NULL);
+
+    clock_t t0, t1;
+    double jit_elapsed = 0.0;
+    double compile_elapsed = 0.0;
+
+    t0 = clock();
     jit_function_compile(function);
-    jit_dump_function(stdout, function, "union-asm");
-    jit_function_apply(function, NULL, NULL);
+    FF compiled_vdbe = jit_function_to_closure(function);
+    t1 = clock();
+    compile_elapsed = ((double) t1-t0)/CLOCKS_PER_SEC;
+    printf("<Benchmarks> JIT Compile time took %.6f seconds !\n", compile_elapsed);
 
-    printf("POP P3 Register: %lld\n", &(p->aMem[1].u));
-    printf("HERE IS RESULT: %lld \n", p->aMem[1].u.i);
+    // Uncomment this to see assembly
+    printf("<Debug> Printing Assembly...\n");
+    jit_dump_function(stdout, function, NULL);
 
+    t0 = clock();
+    compiled_vdbe();
+    t1 = clock();
+    jit_elapsed = ((double) t1-t0)/CLOCKS_PER_SEC;
+    printf("<Benchmarks> JIT execution took %.6f seconds !\n", jit_elapsed);
+
+    free(JIT_registers);
     return function;
-}
-
-void emit_OP_Init(Vdbe* p) {
-    Op *aOp = p->aOp;          /* Copy of p->aOp */
-    Op *pOp = aOp;             /* Current operation */
 }
